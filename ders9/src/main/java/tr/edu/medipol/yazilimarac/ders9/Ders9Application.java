a5bcadf18b8cc13a70b9b41b7f2c1934d6324b3e
bprlab <bprlab@10.201.146.124>
2019-04-27 12:50:09 +0300

    ders 9 springbot ornek

---
+++ ders9/src/main/java/tr/edu/medipol/yazilimarac/ders9/Ders9Application.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimarac.ders9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ders9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ders9Application.class, args);
+	}
+
+}
---
+++ ders9/src/test/java/tr/edu/medipol/yazilimarac/ders9/Ders9ApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.edu.medipol.yazilimarac.ders9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ders9ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
